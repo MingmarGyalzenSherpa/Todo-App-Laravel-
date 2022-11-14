67f23a5ba15543a81fcf12573779b2ed51723b63
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2022-11-14 21:17:32 +0545

    initial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+        TODO LIST
+    </title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body class="bg-dark">
+    <div class="container mt-5 p-4 bg-light rounded">
+
+        <from action="" method="post">
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="floatingInput" placeholder="Enter your task here">
+                <label for="floatingInput">Task</label>
+            </div>
+            <button type="submit" class="btn btn-dark ">Add</button>
+        </from>
+    </div>
+</body>
+
+</html>
